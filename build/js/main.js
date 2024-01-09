d325297ef4e4e61636e302a8c7326471edc152eb
CrimsonCoder42 <nostro37@gmail.com>
2024-01-09 11:46:12 -0500

    final

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,7 @@
 "use strict";
-// index signature
-const todaysTransactions = {
-    Books: -19,
-    Pizza: -5,
-    Job: 50
+const stringEcho = (arg) => arg;
+const isObj = (arg) => {
+    return typeof arg === 'object' && arg !== null;
 };
-const student = {
-    name: 'Jake',
-    GPA: 3.5,
-    classes: [342, 441, 441, 441]
-};
-//console.log(student.test);
-// for (const key in student) {
-//   console.log(`${key}: ${student[key as keyof Student]}`)
-// }
-const logStudentKey = (student, key) => {
-    console.log(`${key}: ${student[key]}`);
-};
-logStudentKey(student, 'GPA');
+console.log(isObj('test'));
+console.log(isObj(['test']));
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,16 @@
-// index signature
+const stringEcho = <T>(arg: T): T => arg;
 
-interface TransactionObj {
-  Pizza: number;
-  Books: number;
-  Job: number;
+
+const isObj = <T>(arg: T): boolean => {
+  return typeof arg === 'object' && arg !== null;
 }
 
-const todaysTransactions: TransactionObj = {
-  Books: -19,
-  Pizza: -5,
-  Job: 50
-}
+console.log(isObj<string>('test'));
+console.log(isObj<string[]>(['test']));
 
-// console.log(todaysTransactions.Pizza);
-// console.log(todaysTransactions['Pizza'])
 
-interface Student {
-  // [key: string]: string | number | number[] | undefined;
-  name: string;
-  GPA: number;
-  classes?: number[];
-}
 
-const student: Student = {
-  name: 'Jake',
-  GPA: 3.5,
-  classes: [342, 441, 441, 441]
-}
 
-//console.log(student.test);
 
-// for (const key in student) {
-//   console.log(`${key}: ${student[key as keyof Student]}`)
-// }
 
-const logStudentKey = (student: Student, key: keyof Student) => {
-  console.log(`${key}: ${student[key]}`)
-}
 
-logStudentKey(student, 'GPA');
